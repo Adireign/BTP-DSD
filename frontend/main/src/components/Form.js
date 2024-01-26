aa35785ac5be851f864a652216a1a206b624fd42
Adish <adishlodha01@gmail.com>
2024-01-27 02:51:11 +0530

    updates

--- frontend/main/src/components/Form.js
+++ frontend/main/src/components/Form.js
@@ -42,7 +42,7 @@ const QuizForm = () => {
 
     try {
       // Send POST request to the Flask server
-      const response = await fetch('http://localhost:5000/submit_quiz', {
+      const response = await fetch('https://btp-dsd-server.vercel.app/submit_quiz', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
